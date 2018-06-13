d8b3cba106d7fb048a78780fce81a72bc006a0ab
Atsushi Watanabe <atsushi.w@ieee.org>
2018-06-13 21:45:17 +0900

    Fix pcl build options

--- check.sh
+++ check.sh
@@ -9,7 +9,7 @@ rm -rf build || true
 mkdir -p build
 cd build
 
-cmake .. -DWITH_LIBUSB=OFF -DWITH_PNG=OFF -DWITH_QHULL=OFF -DWITH_CUDA=OFF -DWITH_VTK=OFF -DWITH_PCAP=OFF -DWITH_OPENGL=OFF -DBUILD_io=OFF -DBUILD_kdtree=OFF -DBUILD_search=OFF -DBUILD_sample_consensus=OFF -DBUILD_2d=OFF -DBUILD_geometry=OFF -DBUILD_features=OFF -DBUILD_ml=OFF -DBUILD_segmentation=OFF -DBUILD_surface=OFF -DBUILD_registration=OFF -DBUILD_keypoints=OFF -DBUILD_tracking=OFF -DBUILD_recognition=OFF -DBUILD_stereo=OFF -DBUILD_tools=OFF
+cmake .. -DWITH_LIBUSB=OFF -DWITH_PNG=OFF -DWITH_QHULL=OFF -DWITH_CUDA=OFF -DWITH_VTK=OFF -DWITH_PCAP=OFF -DWITH_OPENGL=OFF -DBUILD_io=OFF -DBUILD_2d=OFF -DBUILD_geometry=OFF -DBUILD_features=OFF -DBUILD_ml=OFF -DBUILD_segmentation=OFF -DBUILD_surface=OFF -DBUILD_registration=OFF -DBUILD_keypoints=OFF -DBUILD_tracking=OFF -DBUILD_recognition=OFF -DBUILD_stereo=OFF -DBUILD_tools=OFF -DBUILD_filters=ON
 
 make -j
 make install
